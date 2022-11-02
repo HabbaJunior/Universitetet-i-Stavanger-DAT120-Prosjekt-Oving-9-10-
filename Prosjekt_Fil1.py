449a3e56b7908cf1f833c807ea3b9037754d04fc
HabbaJunior <270690@uis.no>
2022-11-02 12:31:09 +0100

    fsefsef

--- Prosjekt_Fil1.py
+++ Prosjekt_Fil1.py
@@ -6,6 +6,7 @@ Created on Wed Oct 26 08:32:17 2022
 
 """
 from datetime import datetime
+InternListeMedAvtaler = []
 
 class avtale:
     def __init__(self, tittel, sted = "Ikke satt", starttidspunkt = "Ikke satt", varighet = -1):
@@ -31,18 +32,16 @@ class avtale:
         self.sted = sted.strip()
     
     def setstarttidspunkt(self, starttidspunkt):
-        self.starttidspunkt = starttidspunkt
-        """
         if starttidspunkt == "Ikke satt":
             self.starttidspunkt = starttidspunkt
-        if isinstance(starttidspunkt, datetime()):
+        if isinstance(starttidspunkt, datetime):
             self.starttidspunkt = starttidspunkt
         else:
             try:
                 self.starttidspunkt = datetime.fromisoformat(starttidspunkt)
             except:
                 raise TypeError("Avtalen forventer \"datetime\" som starttidspunkt")
-        """
+        
     def setvarighet(self, varighet):
         while True:
             try:
@@ -53,7 +52,7 @@ class avtale:
         
         
         
-def nyavtale(liste):
+def nyavtale():
     print("\nHei! Du kan nå lage en ny avtale")
     a = input("Skirv inn navnet til avtalen: ")
     b = input("Skirv inn stedet til avtalen: ")
@@ -72,43 +71,40 @@ def nyavtale(liste):
             d = int(d)
             break
         except:
+            print("Husk korrekt format")
             continue
-    liste = []
-    liste.append(avtale(a, b, c, d))
+    InternListeMedAvtaler.append(avtale(a, b, c, d))
+    return InternListeMedAvtaler
 
 
-def printliste(liste):
-    if isinstance(liste, list):
-        for index in range(len(liste)):
-            print(liste[index] ,"den har index:",  index)
-    else:
-        print("listen finnes ikke")
-
-def eksporterliste(liste, filnavn):
-    if isinstance(liste, list):
+def printliste(liste, Overskrift = "Overskrift :)"):
+    print(Overskrift)    
+    for index in range(len(liste)):
+            print (f"Avtalen: {liste[index]} har index: {index}")
+            
+def eksporterliste(filnavn):
         with open(filnavn, "w", 1, "UTF-8") as ALE:
-            for index in range(len(liste)):
-                ALE.writelines(liste[index].__repr__()+ "\n")
-    else:
-        print("Listen finnes ikke")
+            for index in range(len(InternListeMedAvtaler)):
+                ALE.writelines(InternListeMedAvtaler[index].__repr__()+ "\n")
       
-def importerliste(filnavn, liste):
-    liste = []
+def importerliste(filnavn):
     while True:
         try:
             with open(filnavn, "r", 1, "UTF-8") as ALI:
-                for linje in ALI:
-                    x, y, u, o = linje.split(",")
-                    x = x.strip()
-                    y = y.strip()
-                    u = u.strip()
-                    o = o.strip()
-                    liste.append(x, y, u, o)
-                return liste
-            break
+                pass
         except:
-            print("Filen ikke funnet")
+            print("Filen ikke funnet")  
+        with open(filnavn, "r", 1, "UTF-8") as ALI:    
+            for linje in ALI:
+                x, y, u, o = linje.split(",")
+                x = x.strip()
+                y = y.strip()
+                u = u.strip()
+                o = o.strip()
+                InternListeMedAvtaler.append(x + ", " + y + ", " + u + ", " + o)
+        return InternListeMedAvtaler
         break
+
 
 def avtalerDenneDatoen(ListeMedAvtaler, Dato):
     with open(ListeMedAvtaler, "r", 1, "UTF-8") as LMA:
@@ -135,47 +131,47 @@ def LiknendeTittel(ListeMedAvtaler, søkeord):
                 ListeMedAvtalerMedLiknendeTittel.append(x)
     return ListeMedAvtalerMedLiknendeTittel
 
-def menyen():
+
+while True:
+    print("\nMeny, Velg Tall fra:    1-5")
+    print("Les inn avtaler fra fil: 1 \nSkriv avtalene til fil:  2 \nRegistrer ny avtale:     3 \nPrint alle avtalene:     4 \nAvslutt:                 5")
+    
     while True:
-        print("\nMeny, Velg Tall fra:    1-5")
-        print("Les inn avtaler fra fil: 1 \nSkriv avtalene til fil:  2 \nRegistrer ny avtale:     3 \nPrint alle avtalene:     4 \nAvslutt:                 5")
-        
-        while True:
-            valg = input("Ditt valg:               ")
-            try:
-                valg = int(valg)
-                if 0 < valg <= 5:
-                   break
-                else:
-                    continue
-            except:
-                print("Venligst bruk bare tall mellom 1 og 5")
+        valg = input("Ditt valg:               ")
+        try:
+            valg = int(valg)
+            if 0 < valg <= 5:
+               break
+            else:
                 continue
-     
-        if valg == 1: #Les inn avtaler fra fil
-            navnpafil = input("Skriv inn navnet på filen du vil importere: ")
-            navnpaliste = input("Skriv navet på listen du vil importere til: ")
-            importerliste(navnpafil, navnpaliste)
+        except:
+            print("Venligst bruk bare tall mellom 1 og 5")
+            continue
+ 
+    if valg == 1: #Les inn avtaler fra fil
+        navnpafil = input("Skriv inn navnet på filen du vil importere: ")
+        importerliste(navnpafil+".txt")
         
-        if valg == 2: #Skriv inn avtaler til en fil 
-            navnpaliste = input("Skriv navet på listen du vil eksportere: ")
-            navnpafil = input("Skriv navnet på filen du vil eksportere til: ")
-            eksporterliste(navnpaliste, navnpafil)      
-             
-        if valg == 3: #Registrer ny avtale 
-            liste = input("Hvilken liste vil du legge avtalen til i?: ")
-            nyavtale(liste)
-        
-        if valg == 4: #Print ut avtalene 
-           liste1 = input("Hvilken liste vil du skrive ut?: ") 
-           printliste(liste1)
-           
-        if valg == 5: #avslutt
-            break 
+    
+    if valg == 2: #Skriv inn avtaler til en fil 
+        navnpafil = input("Skriv navnet på filen du vil eksportere til: ")
+        eksporterliste(navnpafil)      
+         
+    if valg == 3: #Registrer ny avtale 
+        nyavtale()
+    
+    if valg == 4: #Print ut avtalene 
+       printliste(InternListeMedAvtaler)
+       
+    if valg == 5: #avslutt
+        break 
                    
 listen = ["apekatt", "jaaa", "ahahah", "rompe"]
+liste2 = []
+#liste2 = importerliste(avtalerlisteeksport)
+#menyen()
 
-menyen()
+
 
 """               
 a = avtale("A", "B","2022-09-16 13:15:12" , 30) 
@@ -191,5 +187,7 @@ x, y, u, o = importerliste("AvtalerListeImport.txt")
 avtale = avtale(x, y, u, o)
 
 o = avtalerDenneDatoen("AvtalerListeImport.txt", "2000-12-12 13:15:12")
+
+Lister[input("Navnet pa listen min: ")] = listex
 """ 
         
